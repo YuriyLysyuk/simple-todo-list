b4b5f3b2ea26b31efa047aabfe9b5022752b5846
lysyuk <lysyuk.y@gmail.com>
2022-02-04 13:59:00 +0300

    Add print all tasks functionality

--- app.js
+++ app.js
@@ -31,5 +31,70 @@ const tasksData = [
 
 // Added self-calling function to protect app variables and functions
 (function (tasksData) {
+  // Tasks container
+  const tasksContainer = document.querySelector('.task-list .list-group');
 
+  // Print all initial tasks
+  tasksContainer.appendChild(getAllTasksHtml(tasksData));
+
+  // Get all tasks html
+  function getAllTasksHtml(tasksData) {
+    // Return undefined if tasks data not received
+    if (!tasksData) {
+      console.error('You need add array with tasks data');
+      return;
+    }
+
+    // Create document fragment for future tasks list
+    const fragment = document.createDocumentFragment();
+
+    // Get html markup for all tasks
+    tasksData.forEach((task) => {
+      // Create html for one task
+      const li = getOneTaskHtml(task);
+      // Append one task to fragment
+      fragment.appendChild(li);
+    });
+
+    return fragment;
+  }
+
+  // Get html for one task
+  function getOneTaskHtml({ title, description }) {
+    // Create li element
+    const li = document.createElement('li');
+    // Add li classes
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'gap-2'
+    );
+
+    // Create task body div, add class and text
+    const taskBody = document.createElement('div');
+    taskBody.classList.add('task-body', 'me-auto');
+    taskBody.textContent = description;
+
+    // Create task title div, add classes and text
+    const taskTitle = document.createElement('div');
+    taskTitle.classList.add('task-title', 'fw-bold');
+    taskTitle.textContent = title;
+
+    // Insert title to task body
+    taskBody.insertAdjacentElement('afterbegin', taskTitle);
+
+    // Create delete button, add classes and text
+    const btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-danger', 'btn-sm', 'align-self-center');
+    btn.setAttribute('type', 'button');
+    btn.insertAdjacentHTML('afterbegin', 'Delete&nbsp;task');
+
+    // Add markup to li
+    li.appendChild(taskBody);
+    li.appendChild(btn);
+
+    return li;
+  }
 })(tasksData);
